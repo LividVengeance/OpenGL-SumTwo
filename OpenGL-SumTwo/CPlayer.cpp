f9719f9f8d6fd618cb314dc8b78d2d3656db3efc
Andrew (Home Desktop) <55672451+LividVengeance@users.noreply.github.com>
2020-05-03 19:17:22 +1200

    - CPlayer now is locked to game space

--- OpenGL-SumTwo/CPlayer.cpp
+++ OpenGL-SumTwo/CPlayer.cpp
@@ -17,25 +17,40 @@ CPlayer::~CPlayer()
 
 void CPlayer::moveInput(GLfloat deltaTime)
 {
+	int playerLimitHorzontal = (Utils::SCR_WIDTH / 2) - 25;
+	int playerLimitVertical = (Utils::SCR_HEIGHT / 2) - 25;
 	// Move Right
 	if (PlayerInputs->getKeyState('D') == true || PlayerInputs->getKeyState('d') == true)
 	{
-		playerMesh->objPosition.x += 200.0f * deltaTime;
+		if (!(playerMesh->objPosition.x > playerLimitHorzontal)) // 25 is half player mesh size
+		{
+			playerMesh->objPosition.x += 200.0f * deltaTime;
+		}
 	}
 	// Move Left
 	if (PlayerInputs->getKeyState('A') == true || PlayerInputs->getKeyState('a') == true)
 	{
-		playerMesh->objPosition.x -= 200.0f * deltaTime;
+		
+		if (!(playerMesh->objPosition.x < -playerLimitHorzontal))
+		{
+			playerMesh->objPosition.x -= 200.0f * deltaTime;
+		}
 	}
 	// Move Up
 	if (PlayerInputs->getKeyState('W') == true || PlayerInputs->getKeyState('w') == true)
 	{
-		playerMesh->objPosition.y += 200.0f * deltaTime;
+		if (!(playerMesh->objPosition.y > playerLimitVertical))
+		{
+			playerMesh->objPosition.y += 200.0f * deltaTime;
+		}
 	}
 	// Move Up
 	if (PlayerInputs->getKeyState('S') == true || PlayerInputs->getKeyState('s') == true)
 	{
-		playerMesh->objPosition.y -= 200.0f * deltaTime;
+		if (!(playerMesh->objPosition.y < -playerLimitVertical))
+		{
+			playerMesh->objPosition.y -= 200.0f * deltaTime;
+		}
 	}
 	// Shoot
 	if (PlayerInputs->getKeyState(' ') == true)
